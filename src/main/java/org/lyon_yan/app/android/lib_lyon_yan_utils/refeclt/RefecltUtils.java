a98ee8d84408c7e56166b46ee42b6b85d6d4b70f
Lyon1994 <765211630@qq.com>
2016-02-18 10:34:49 +0800

    支付功能整合 第二阶段，支付宝的整合基本完成 下一阶段将进行支付宝与数据订单的保存的整合 注意：必须保证支付方式的可扩展性
    支付宝完成对接

--- src/main/java/org/lyon_yan/app/android/lib_lyon_yan_utils/refeclt/RefecltUtils.java
+++ src/main/java/org/lyon_yan/app/android/lib_lyon_yan_utils/refeclt/RefecltUtils.java
@@ -1,49 +1,93 @@
 package org.lyon_yan.app.android.lib_lyon_yan_utils.refeclt;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * refelect工具包
- * 
+ *
  * @author Lyon_Yan <br>
  *         <b>time</b>: 2015年10月31日 下午3:35:27
  */
 public class RefecltUtils {
 
-	private static final String SET_ = "set";
-	private static final String GET_ = "get";
+    private static final String SET_ = "set";
+    private static final String GET_ = "get";
 
-	/**
-	 * 将属性的首字符大写，方便构造get，set方法
-	 * 
-	 * @author Lyon_Yan <br>
-	 *         <b>time</b>: 2015年10月31日 下午3:35:46
-	 * @param name
-	 * @return
-	 */
-	public static String returnSetName(String name) {// 将属性的首字符大写，方便构造get，set方法
-		return SET_ + returnUpperCaseFirstChar(name);
-	}
+    /**
+     * 将属性的首字符大写，方便构造get，set方法
+     *
+     * @param name
+     * @return
+     * @author Lyon_Yan <br>
+     * <b>time</b>: 2015年10月31日 下午3:35:46
+     */
+    public static String returnSetName(String name) {// 将属性的首字符大写，方便构造get，set方法
+        return SET_ + returnUpperCaseFirstChar(name);
+    }
 
-	/**
-	 * 将属性的首字符大写，方便构造get，set方法
-	 * 
-	 * @author Lyon_Yan <br>
-	 *         <b>time</b>: 2015年10月31日 下午3:35:46
-	 * @param name
-	 * @return
-	 */
-	public static String returnGetName(String name) {// 将属性的首字符大写，方便构造get，set方法
-		return GET_ + returnUpperCaseFirstChar(name);
-	}
+    /**
+     * 将属性的首字符大写，方便构造get，set方法
+     *
+     * @param name
+     * @return
+     * @author Lyon_Yan <br>
+     * <b>time</b>: 2015年10月31日 下午3:35:46
+     */
+    public static String returnGetName(String name) {// 将属性的首字符大写，方便构造get，set方法
+        return GET_ + returnUpperCaseFirstChar(name);
+    }
 
-	/**
-	 * 将属性的首字符大写，方便构造get，set方法
-	 * 
-	 * @author Lyon_Yan <br>
-	 *         <b>time</b>: 2015年11月3日 上午11:03:52
-	 * @param name
-	 * @return
-	 */
-	public static String returnUpperCaseFirstChar(String name) {
-		return name.substring(0, 1).toUpperCase() + name.substring(1);
-	}
+    /**
+     * 将属性的首字符大写，方便构造get，set方法
+     *
+     * @param name
+     * @return
+     * @author Lyon_Yan <br>
+     * <b>time</b>: 2015年11月3日 上午11:03:52
+     */
+    public static String returnUpperCaseFirstChar(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
+    }
+
+    /**
+     * 获取泛型
+     *
+     * @param field
+     * @return
+     */
+    public static List<Class> getGenericClassesFromType(Field field) {
+        field.setAccessible(true);
+        String genericTypes = field.getGenericType().toString().replace(field.getType().getName(), "").replace("<", "").replace(">", "");
+        genericTypes = genericTypes.replace(" ", "");
+        List<Class> classes = new ArrayList<>();
+        try {
+            if (genericTypes.contains(",")) {
+                String[] clazzes = genericTypes.split(",");
+                for (String clazz : clazzes) {
+                    classes.add(Class.forName(clazz));
+                }
+            } else {
+                classes.add(Class.forName(genericTypes));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return classes;
+    }
+
+    /**
+     * 获取泛型
+     *
+     * @param field
+     * @return
+     */
+    public static Class getGenericClassFromType(Field field) {
+        List<Class> classes = getGenericClassesFromType(field);
+        if (classes.size() > 0) {
+            return classes.get(0);
+        }
+        return null;
+    }
 }
--- src/main/java/org/lyon_yan/app/android/lib_lyon_yan_utils/window/WindowUtils.java
+++ src/main/java/org/lyon_yan/app/android/lib_lyon_yan_utils/window/WindowUtils.java
@@ -3,10 +3,12 @@ package org.lyon_yan.app.android.lib_lyon_yan_utils.window;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -77,5 +79,40 @@ public class WindowUtils {
 
 	public static boolean isAndroid5() {
 		return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+	}
+
+	/**
+	 * 判断是否为平板
+	 * @param context
+	 * @return
+	 */
+	public static boolean isTablet(Context context) {
+		return (context.getResources().getConfiguration().screenLayout
+				& Configuration.SCREENLAYOUT_SIZE_MASK)
+				>= Configuration.SCREENLAYOUT_SIZE_LARGE;
+	}
+	/**
+	 * 判断是否为平板
+	 *
+	 * @return
+	 */
+	private static boolean isPad(Context context) {
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		// 屏幕宽度
+		float screenWidth = display.getWidth();
+		// 屏幕高度
+		float screenHeight = display.getHeight();
+		DisplayMetrics dm = new DisplayMetrics();
+		display.getMetrics(dm);
+		double x = Math.pow(dm.widthPixels / dm.xdpi, 2);
+		double y = Math.pow(dm.heightPixels / dm.ydpi, 2);
+		// 屏幕尺寸
+		double screenInches = Math.sqrt(x + y);
+		// 大于6尺寸则为Pad
+		if (screenInches >= 6.0) {
+			return true;
+		}
+		return false;
 	}
 }
